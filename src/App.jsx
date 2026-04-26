76398485bff5ed307b703433e692e31b4f0aaa76
Aobakwe Modillane <aobakwemodillane27@gmail.com>
2026-04-26 15:33:57 +0200

    Fix import statement for Certifications component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Chatbot from "./components/Chatbot";
-import Certifications from "./components/Certifications";
+
 
 const App = () => {
   return (
